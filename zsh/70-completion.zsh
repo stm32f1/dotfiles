5de6f2d43ce242a6925008e43c56679958f60f07
stm32f1 <e129.a1@gmail.com>
2016-12-20 22:10:04 +0900

    update completion setting

--- zsh/70-completion.zsh
+++ zsh/70-completion.zsh
@@ -4,7 +4,8 @@ function zle-line-init () {
 	auto-fu-init
 }
 zle -N zle-line-init
-zstyle ':completion:*' completer _oldlist _complete _approximate
+zstyle ':completion:*' completer _complete _match _oldlist _approximate
+zstyle ':auto-fu:var' postdisplay $''
 
 # 補完の表示方法を変更する
 #zstyle ':completion:*' completer _complete _match _approximate
